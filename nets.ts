5191ac92745daf4d9f0d3206e62a82e0317e313a
vjjft <vjjft@vjjft.com>
2023-07-10 19:43:45 -0400

    removes unused bin & net; bumps polkadot to v0.9.40

--- nets.ts
+++ nets.ts
@@ -1,15 +1,9 @@
-
 import { bins, net } from "capi/nets"
 
 const bin = bins({
-  polkadot: ["polkadot-fast", "v0.9.38"],
-  polkadotParachain: ["polkadot-parachain-fast", "v0.9.380"],
+  polkadot: ["polkadot-fast", "v0.9.40"],
 })
 
-export const polkadotDev = net.dev({
-  bin: bin.polkadot,
-  chain: "polkadot-dev",
-})
 export const polkadot = net.ws({
   url: "wss://rpc.polkadot.io/",
   version: "v0.9.40",
