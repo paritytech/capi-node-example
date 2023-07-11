3e10c4ea602b1931720b1cd33215a60b7692b495
vjjft <vjjft@vjjft.com>
2023-07-11 08:29:14 -0400

    adds dev target for polkadot; changes npm scripts relatedly

--- nets.ts
+++ nets.ts
@@ -7,4 +7,10 @@ const bin = bins({
 export const polkadot = net.ws({
   url: "wss://rpc.polkadot.io/",
   version: "v0.9.40",
+  targets: {
+    dev: net.dev({
+      bin: bin.polkadot,
+      chain: "polkadot-dev",
+    }),
+  },
 })
