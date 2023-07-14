24a20a8c8a6ec02ca5b4982b57be290860100ad5
vjjft <vjjft@vjjft.com>
2023-07-14 18:30:32 -0400

    removed errant semis added by local editor

--- nets.ts
+++ nets.ts
@@ -1,15 +1,15 @@
-import { bins, net } from "capi/nets";
+import { bins, net } from "capi/nets"
 
 const bin = bins({
   polkadot: ["polkadot-fast", "v0.9.38"],
-});
+})
 
 export const polkadotDev = net.dev({
   bin: bin.polkadot,
   chain: "polkadot-dev",
-});
+})
 
 export const polkadot = net.ws({
   url: "wss://rpc.polkadot.io/",
   targets: { dev: polkadotDev },
-});
+})
